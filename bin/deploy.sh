1def612c02a62e0d26fdf2ec601040f80b5f02be
EslamAref75 <EslamAref75>
2022-02-05 21:10:11 +0200

    initial commit

---
+++ bin/deploy.sh
@@ -0,0 +1,2 @@
+cd udagram-frontend && aws s3 cp --recursive --acl public-read ./www s3://udagram-bucket2022/
+
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://udagram-api-dev22.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
