16bd7eacdef2d18ade02ccc9fdd0308687350b8d
EslamAref75 <EslamAref75>
2022-02-07 23:59:59 +0200

    circleci

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-sup2m3sr.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-sup2m3sr.us-east-1.elasticbeanstalk.com/api/v0"
 };
 
 /*
