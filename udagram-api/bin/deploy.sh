7fd57a62169bfe2d9402587d70ed8af3fa80ed05
EslamAref75 <EslamAref75>
2022-02-07 16:42:33 +0200

    circleci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
 eb create udagram-api-dev
-eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=postgres DB_PORT=5432 PORT=8081 POSTGRES_HOST=database-1.cqldkqnd68am.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=udagram-bucket2022 URL=http://udagram-bucket2022.s3-website-us-east-1.amazonaws.com JWT_SECRET=new_password
-eb deploy
+&& eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=postgres DB_PORT=5432 PORT=8081 POSTGRES_HOST=database-1.cqldkqnd68am.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=udagram-bucket2022 URL=http://udagram-bucket2022.s3-website-us-east-1.amazonaws.com JWT_SECRET=new_password
+&& eb deploy
